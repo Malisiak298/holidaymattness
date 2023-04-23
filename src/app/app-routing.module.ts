c937355768a9dd535ba763e9fcfe7f86998b6807
Matt <matt.malisiak@gmail.com>
2023-04-22 23:57:31 -0400

    initial architecture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './features/homepage/homepage.component';
+import { TutorialsComponent } from './features/tutorials/tutorials.component';
+import { VideosComponent } from './features/videos/videos.component';
+import { AboutComponent } from './features/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomepageComponent,
+  },
+  {
+    path: 'tutorials',
+    component: TutorialsComponent,
+  },
+  {
+    path: 'videos',
+    component: VideosComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
